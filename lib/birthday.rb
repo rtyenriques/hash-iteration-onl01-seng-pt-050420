493cce7906939f8143944ab88cc3eb10a9dab4a0
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-14 09:51:32 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,8 +7,19 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kid, age|
-    
+
+
     puts "Happy Birthday #{kid}! You are now #{age} years old!"
+
+  end
 end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if(age < 10)
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
   end
 end
